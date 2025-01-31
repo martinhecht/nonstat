516e9884b332df91115775f769afca8b2c3cc568
martinhecht <martin.hecht@hsu-hh.de>
2025-02-01 00:38:30 +0100

    MH 0.0.3 2025-01-31

--- R/is.nonstat.R
+++ R/is.nonstat.R
@@ -1,5 +1,6 @@
 ## Changelog:
 # MH 0.0.1 2025-01-29: initial
+# MH 0.0.3 2025-01-31: mod and checks on time series epoch length
 
 ## Documentation
 #' @title Test for nonstationarity
@@ -10,6 +11,8 @@
 #' @param cut.psr2 threshold for PSR value that indicates variance non-stationarity
 #' @param span numerical value that is passed to the \code{loess} function
 #' @return a logical scalar indicating whether the time series is stationary (\code{TRUE}) or not (\code{FALSE})
+#' @references
+#' Zitzmann, S., Lindner, C., Lohmann, J. F., & Hecht, M. (2024). A novel nonvisual procedure for screening for nonstationarity in time series as obtained from intensive longitudinal designs [Preprint]. https://www.researchgate.net/publication/384354932_A_Novel_Nonvisual_Procedure_for_ Screening_for_Nonstationarity_in_Time_Series_as_Obtained_from_Intensive_Longitudinal_Designs
 #' @examples
 #' set.seed( 1234 )
 #' x <- rnorm( 100 )
@@ -17,19 +20,47 @@
 
 ## Function definition
 is.nonstat <- function( tseries, nEp=2, cut.psr1=1.10, cut.psr2=1.01, span=3 ){
+	
+	# MH 0.0.3 2025-01-31: mod on time series epoch length
+	# loop to remove elements until the length is both divisible by nEp and greater than nEp
+	while (length(tseries) %% nEp != 0 && length(tseries) > 3*nEp) {
+		tseries <- tseries[-length(tseries)]  # Remove last element
+	}
+
+	if( length(tseries) %% nEp == 0 && length( tseries ) >= 3*nEp ){
+
+		psr1 <- try( psr1( tseries=tseries, nEp=nEp ) )
+		psr2 <- try( psr2( tseries=tseries, nEp=nEp, span=span ) )
+
+		if( inherits( psr1, "try-error" ) ) psr1 <- NULL
+		if( inherits( psr2, "try-error" ) ) psr2 <- NULL
 		
-	psr1 <- psr1( tseries=tseries, nEp=nEp )
-	psr2 <- psr2( tseries=tseries, nEp=nEp, span=span )
-	
-	nonstat.mean <- psr1 > cut.psr1
-	nonstat.var <- psr2 > cut.psr2
-	
-	ret <- nonstat.mean | nonstat.var
-	
-	attr(ret, "psr1") <- psr1
-	attr(ret, "nonstat.mean") <- nonstat.mean
-	attr(ret, "psr2") <- psr2
-	attr(ret, "nonstat.var") <- nonstat.var
+		if( !is.null( psr1 ) && !is.na( psr1 ) && !is.infinite( psr1 ) ) nonstat.mean <- psr1 > cut.psr1 else {
+			nonstat.mean <- NULL
+			warning( "Unable to assess the nonstationarity of the mean." )
+		}
+		if( !is.null( psr2 ) && !is.na( psr2 ) && !is.infinite( psr2 ) ) nonstat.var  <- psr2 > cut.psr2 else {
+			nonstat.var  <- NULL
+			warning( "Unable to assess the nonstationarity of the variance." )
+		}
+		
+		if( !is.null( nonstat.mean ) && !is.null( nonstat.var ) ){
+			ret <- nonstat.mean | nonstat.var
+		} else {
+			ret <- NULL
+		}
+
+		if( !is.null( ret ) ){
+			attr(ret, "psr1") <- psr1
+			attr(ret, "nonstat.mean") <- nonstat.mean
+			attr(ret, "psr2") <- psr2
+			attr(ret, "nonstat.var") <- nonstat.var
+		}
+
+	} else {
+		ret <- NULL
+		warning("The length of 'tseries' should be at least 3 times 'nEp', and it must be divisible by 'nEp' without a remainder.")
+	}
 
 	return( ret )
 }
@@ -48,6 +79,10 @@ is.nonstat <- function( tseries, nEp=2, cut.psr1=1.10, cut.psr2=1.01, span=3 ){
 # set.seed( 1234 )
 # x <- rnorm( 100 )
 # is.nonstat( x )
+
+# set.seed( 1234 )
+# x <- rnorm( 15 )
+# is.nonstat( x, nEp=5 )
 
 
 
